1ea7e47f2ee891346531165b6504184adbf0899e
emnificent <49172724+emnificent@users.noreply.github.com>
2024-03-03 02:10:32 +0100

    feat: add Domains listing

--- src/lib/components/Domains.svelte
+++ src/lib/components/Domains.svelte
@@ -1,12 +1,51 @@
-<article>
-  <h2><a href="https://dan.com/domain-seller/emilie" target="_blank">Expiring Domains</a></h2>
-  <p>When I rename or give up on a project, I wish I could release the domain immediately, selling it is the closest I can get to that; <a href="https://dan.com/domain-seller/emilie" target="_blank">check out my store</a>.</p>
-</article>
+<script>
+  let domainsList = [
+    'blackcats.party',
+    'bum.su',
+    'emilie.fun',
+    'emilie.pm',
+    'e-pals.party',
+    'lust.sx',
+    'lustess.com',
+    'lustfulslut.com',
+    'testtube.work'
+  ]
+
+  // Fisher-Yates x Durstenfeld
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  shuffle(domainsList);
+</script>
+
+<section>
+  <h2>Expiring Domains</h2>
+  <ul id="domains-list">
+    { #each domainsList as domainName }
+      <li><a href="https://${domainName}" target="_blank">{domainName}</a></li>
+    { /each }
+  </ul>
+</section>
 
 <style>
   h2 {
     font-weight: var(--fw--bold);
     font-size: calc(var(--fs-title) * 1rem);
     margin-bottom: 0.25rem;
+  }
+
+  ul {
+    list-style-position: inside;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  a {
+    font-family: monospace;
   }
 </style>
